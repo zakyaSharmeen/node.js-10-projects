32014dc01f019551b2123e758b494bfa9d30940e
zakya sharmeen <sakarim9124@gmail.com>
2025-02-27 23:25:27 +0530

    food-delivery-app

--- p-10-food-deleivery-app/backend/db.js
+++ p-10-food-deleivery-app/backend/db.js
@@ -9,23 +9,18 @@ const mongoURL = "mongodb+srv://admin:adminFood123@food-app.7biyr.mongodb.net/fo
 
 const mongoDB = async () => {
     try {
-        // Connect to the database
         await mongoose.connect(mongoURL);
         console.log("MongoDB connected successfully");
 
-        // Wait for the database connection to be fully established
         const db = mongoose.connection.db;
 
-        // Fetch data from the 'food' collection
         const foodCollection = db.collection("food");
 
-        // Ensure the collection exists and fetch data
         const data = await foodCollection.find({}).toArray();
 
         if(!data){
             console.log(error);
         }else{
-            // Assign to global variable
         global.food = data;
         console.log('====================================');
         // console.log(global.food);
